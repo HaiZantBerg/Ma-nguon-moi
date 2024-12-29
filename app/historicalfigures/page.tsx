12fbf20ec88613afe43f74217c89d05ca1b9fd96
HaiZantBerg <tandocong5@gmail.com>
2024-12-29 19:06:25 +0700

    wtf config git kho vcl

--- app/historicalfigures/page.tsx
+++
@@ -1,6 +0,0 @@
-import SpaceCanvas from "@/components/Main/SpaceCanvas";
-import React from "react";
-
-export default function page() {
-    return <SpaceCanvas />;
-}
--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function page() {
-    return (
-        <div className="w-screen h-screen flex justify-center items-center">
-            <div>helo</div>
-        </div>
-    );
-}
