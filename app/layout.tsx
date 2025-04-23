086d0d531aea56fd62e5c1515132a10999eb34b0
HaiZantBerg <tandocong5@gmail.com>
2025-04-23 12:09:46 +0700

    asasfsaf

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} overflow-auto`}>
+            <body className={`${inter.className} overflow-auto h-svh`}>
                 <QueryProvider>
                     <Navbar />
                     {children}
