868185ed2f159b9c00e0a4773210b0a90aaa0c62
HaiZantBerg <tandocong5@gmail.com>
2025-05-05 00:22:39 +0700

    thay đổi hoàn toàn, đập đi xây lại

--- src/components/Extra/static.ts
+++
@@ -1,8 +0,0 @@
-export const extraBodyVariants: { [key: string]: string } = {
-    instance: "sm:ml-[40px] ml-[28px]",
-    content: "ml-4",
-    normal: "lg:ml-0 md:ml-1 ml-2",
-};
-
-export const extraPaddingLeft: string =
-    "md1:pl-[81px] lg:pl-[30px] md:pl-[26px] pl-[12px]";
--- src/components/Main/FunFacts.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function FunFacts() {
-    return <div>FunFacts</div>;
-}
