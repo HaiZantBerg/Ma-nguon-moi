89fc91de9e929cf68ae31c6500b2822525140b27
HaiZantBerg <tandocong5@gmail.com>
2025-06-06 18:24:15 +0700

    dmm prisma

--- emails/ResetPasswordTemplate.tsx
+++ emails/ResetPasswordTemplate.tsx
@@ -22,7 +22,7 @@ export default function ResetPassword({
         <Html>
             <Head />
             <Tailwind>
-                <Container className="font-sans mx-auto max-w-[30rem] my-[4rem] border border-solid px-[2rem] pt-[3rem] pb-[2rem] rounded-2xl border-neutral-700">
+                <Container className="font-sans text-black bg-white mx-auto max-w-[30rem] my-[4rem] border border-solid px-[2rem] pt-[3rem] pb-[2rem] rounded-2xl border-neutral-700">
                     <Section>
                         <Link
                             href="https://ethems.com"
@@ -45,19 +45,25 @@ export default function ResetPassword({
                         <header className="font-bold text-[1.35rem] font-['Chakra_Petch']">
                             Đặt lại mật khẩu
                         </header>
-                        <p>
+                        <p className="mb-6">
                             Chúng tôi đã nhận được yêu cầu đặt lại mật khẩu cho
-                            tài khoản Ethems được liên kết với {email}.
+                            tài khoản Ethems được liên kết với{" "}
+                            <span className="text-black no-underline">
+                                {email}
+                            </span>
+                            .
                             <br />
                             Để đặt lại mật khẩu, hãy nhấp vào nút bên dưới.
                         </p>
-                        <Link
-                            className="rounded-lg py-[0.75rem] px-[1rem] bg-[#19026e] text-white font-medium cursor-pointer"
-                            href={`https://www.ethems.com/reset-password?token=${resetPasswordToken}`}
-                        >
-                            Đặt lại mật khẩu
-                        </Link>
-                        <p>
+                        <div>
+                            <Link
+                                className="rounded-lg py-[0.75rem] px-[1rem] bg-[#19026e] text-white font-medium cursor-pointer"
+                                href={`https://www.ethems.com/reset-password?token=${resetPasswordToken}`}
+                            >
+                                Đặt lại mật khẩu
+                            </Link>
+                        </div>
+                        <p className="mt-6">
                             Nếu bạn không yêu cầu đặt lại mật khẩu, bạn có thể
                             bỏ qua email này.
                         </p>
